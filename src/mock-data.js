e259d3b5b967aa032b070a1b33eca6fe5d109976
Kirsten Kerksiek <kirstenkerksiek@gmail.com>
2020-03-19 16:54:13 -0600

    shorten KK

--- src/mock-data.js
+++ src/mock-data.js
@@ -3,72 +3,72 @@ let mock = [{
     name: "Hike the Y",
     description: 'Outdoor Hiking Activity',
     price: "$0",
-    location: "Provo, UT",
-    image: 'hike-the-y.jpg' //will put in actual image
+    location: "Provo",
+    image: 'hike-the-y.jpg'
   },
   {
     id: 2,
-    name: "Don Joaquin Street Tacos",
+    name: "Don Joaquin's",
     description: 'Mexican Fast Food',
     price: "$15",
-    location: "Provo, UT",
-    image: 'don-joaquin.jpg' //will put in actual image
+    location: "Provo",
+    image: 'don-joaquin.jpg'
   },
   {
     id: 3,
     name: "Peaks Ice Arena",
     description: 'Indoor Ice Skating',
     price: "$15",
-    location: "Provo, UT",
-    image: 'peaks-ice.jpg' //will put in actual image
+    location: "Provo",
+    image: 'peaks-ice.jpg'
   },
   {
     id: 4,
-     name: "Squaw Peak Overlook",
+     name: "Squaw Peak",
     description: 'Scenery ;)',
     price: "$0",
-    location: "Provo, UT",
-    image: 'squaw-peak.jpg' //will put in actual image
+    location: "Provo",
+    image: 'squaw-peak.jpg'
   },
   {
     id: 5,
     name: "Soap Factory",
     description: 'Indoors, make soap',
     price: "$15",
-    location: "Provo, UT",
-    image: 'soap-factory.jpg' //will put in actual image
+    location: "Provo",
+    image: 'soap-factory.jpg'
   },
   {
     id: 6,
     name: "KoKo’s Lunchbox",
     description: 'Korean Food',
     price: "$20",
-    location: "Provo, UT",
-    image: 'koko-png.jpg' //will put in actual image
+    location: "Provo",
+    image: 'koko-png.jpg'
   },
   {
     id: 7,
     name: "Nickel City",
     description: 'Nickel Arcade',
     price: "$10",
-    location: "Orem, UT",
-    image: 'nickel-city.jpg' //will put in actual image
+    location: "Orem",
+    image: 'nickel-city.jpg'
   },
   {
     id: 8,
     name: "Bridal Veil Falls",
     description: 'Hiking Waterfalls',
     price: "$0",
-    location: "Provo, UT",
-    image: 'bridal-veil.jpg' //will put in actual image
+    location: "Provo",
+    image: 'bridal-veil.jpg'
   },
   {
     id: 9,
    name: "BYU Bell Tower",
     description: 'BYU, Sit and Talk Spot ;)',
     price: "$0",
-    location: "Provo, UT",
-    image: 'bell-tower.jpg' //will put in actual image
+    location: "Provo",
+    image: 'bell-tower.jpg'
 
   },
   {
@@ -76,128 +76,128 @@ let mock = [{
     name: "Wilk Cougareat",
     description: 'BYU Fast Food',
     price: "$15",
-    location: "Provo, UT",
-    image: 'wilk-cougareat.jpg' //will put in actual image
+    location: "Provo",
+    image: 'wilk-cougareat.jpg'
   },
   {
     id: 11,
     name: "Swig",
     description: 'Soda Drinks & Food',
     price: "$10",
-    location: "Provo, UT",
-    image: 'swig.jpg' //will put in actual image
+    location: "Provo",
+    image: 'swig.jpg'
   },
   {
     id: 12,
     name: "DI Dress Up",
     description: 'Buy each other an outfit',
     price: "$10",
-    location: "Provo, UT",
-    image: 'di.jpg' //will put in actual image
+    location: "Provo",
+    image: 'di.jpg'
   },
   {
     id: 13,
-    name: "Mona Lake Swings ",
+    name: "Mona Lake",
     description: 'Swimming, picnic, and swings',
     price: "$0",
-    location: "Mona, UT",
-    image: 'mona-swings.jpg' //will put in actual image
+    location: "Mona",
+    image: 'mona-swings.jpg'
   },
   {
     id: 14,
     name: "Vivian Park",
     description: 'hiking, fishing, chilling',
     price: "$0",
-    location: "Provo, UT",
-    image: 'vivian-park.jpg' //will put in actual image
+    location: "Provo",
+    image: 'vivian-park.jpg'
   },
   {
     id: 15,
-    name: "Fifth Water Hot Springs",
+    name: "Hot Springs",
     description: 'Hiking, hot springs, outdoors',
     price: "$0",
-    location: "Spanish Fork, UT",
-    image: 'hot-springs.jpg' //will put in actual image
+    location: "Spanish Fork",
+    image: 'hot-springs.jpg'
   },
   {
     id: 16,
     name: "Asa Ramen",
     description: 'Japanese Ramen Food',
     price: "$15",
-    location: "Orem, UT",
-    image: 'asa-ramen.jpg' //will put in actual image
+    location: "Orem",
+    image: 'asa-ramen.jpg'
   },
   {
     id: 17,
     name: "Provo Temple",
     description: 'Walk around the grounds',
     price: "$0",
-    location: "Orem, UT",
-    image: 'provo-temple.jpg' //will put in actual image
+    location: "Orem",
+    image: 'provo-temple.jpg'
   },
   {
     id: 18,
-    name: "Rock Canyon Park",
-    description: 'Hammocking',
+    name: "Rock Canyon",
+    description: 'Hammocking Park',
     price: "$0",
-    location: "Orem, UT",
-    image: 'rock-canyon.jpg' //will put in actual image
+    location: "Orem",
+    image: 'rock-canyon.jpg'
   },
   {
     id: 19,
     name: "Provo Canyon",
     description: 'Drive and talk ;)',
     price: "$0",
-    location: "Provo, UT",
-    image: 'provo-canyon.jpg' //will put in actual image
+    location: "Provo",
+    image: 'provo-canyon.jpg'
   },
   {
     id: 20,
     name: "Downtown Provo",
     description: 'Shopping, walking, talking, and food',
     price: "$15",
-    location: "Provo, UT",
-    image: 'downtown-provo.jpg' //will put in actual image
+    location: "Provo",
+    image: 'downtown-provo.jpg'
   },
   {
     id: 21,
     name: "University Mall",
     description: 'Food, murals, shopping, walking, talking',
     price: "$15",
-    location: "Orem, UT",
-    image: 'university-mall.jpg' //will put in actual image
+    location: "Orem",
+    image: 'university-mall.jpg'
   },
   {
     id: 22,
     name: "Bean Museum",
     description: 'BYU, Walk around and look at dead animals',
     price: "$0",
-    location: "Provo, UT",
-    image: 'bean-musuem.jpg' //will put in actual image
+    location: "Provo",
+    image: 'bean-musuem.jpg'
   },
   {
     id: 23,
     name: "Museum of Art",
     description: 'BYU, Walk around and enjoy art',
     price: "$0",
-    location: "Provo, UT",
-    image: 'moa.jpg' //will put in actual image
+    location: "Provo",
+    image: 'moa.jpg'
   },
   {
     id: 24,
     name: "Good Move Cafe",
     description: 'Eat food and play board games',
     price: "$30",
-    location: "Provo, UT",
-    image: 'good-move.jpg' //will put in actual image
+    location: "Provo",
+    image: 'good-move.jpg'
   },
   {
     id: 25,
-    name: "Loveland Living Planet Aquarium",
+    name: "The Aquarium",
     description: 'See the animals that live in the sea',
     price: "$35",
-    location: "Provo, UT",
-    image: 'loveland.jpg' //will put in actual image
+    location: "Provo",
+    image: 'loveland.jpg'
   },
   ]
 
