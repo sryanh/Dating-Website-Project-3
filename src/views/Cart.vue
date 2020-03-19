96cb6009a1f84f608eb3527a7eb96008c9ad44d9
kirstenkerksiek <55109006+kirstenkerksiek@users.noreply.github.com>
2020-03-19 17:45:10 -0600

    Wishlist

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,25 +1,29 @@
 <template>
 <div>
   <div class="wrapper">
-      <form class="pure-form">
-      </form>
+    <form class="pure-form">
+    </form>
   </div>
   <div class="CartMessage">
-<!-- put vif here and velse in another div-->
-<div class="ifStatement" v-if="this.$root.$data.cart.length === 0">Nothing Here!</div>
-<div class="ElseStatment" v-else>
-    <ul v-for="product in this.$root.$data.cart" :key="product.id">
-      <div class = 'dateHeart'>{{product.name}}
-        <div class="image">
-          <img :src="'/images/products/'+product.image">
+    <!-- put vif here and velse in another div-->
+    <div class="ifStatement" v-if="this.$root.$data.cart.length === 0">Nothing Here!</div>
+    <div class="ElseStatment" v-else>
+      <ul v-for="product in this.$root.$data.cart" :key="product.id">
+        <div class='dateHeart'><button v-on:click="RemoveProduct(product)">
+          <div class= 'name'>
+              {{product.name}}
+            </div>
+          <div class="image">
+            <img :src="'/images/products/'+product.image">
+          </div>
+          <p> {{product.description}} </p>
+          <i class="fas fa-times"></i></button>
+          <!-- add remove button here with function  *** IF AN ISSUE COMES v-bind:for product-->
         </div>
-        <!-- add remove button here with function  *** IF AN ISSUE COMES v-bind:for product-->
-          <button v-on:click="RemoveProduct(product)"><i class="fas fa-times"></i></button>
-      </div>
-     </ul>
-   </div>
-   </div>
+      </ul>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -34,9 +38,8 @@ export default {
       searchText: '',
     }
   },
-  methods:{
-    RemoveProduct(product)
-    {
+  methods: {
+    RemoveProduct(product) {
       this.$root.$data.cart.splice(this.$root.$data.cart.indexOf(product), 1);
     }
   }
@@ -54,6 +57,16 @@ export default {
   border: 1px solid #ccc;
   border-radius: 4px;
   width: 50%;
+}
+
+.name {
+font-size: 25px;
+}
+
+button {
+  background-color: #DFBBB6;
+  padding: 5px;
+  font-size: 20px;
 }
 
 form {
@@ -80,17 +93,17 @@ input {
   text-align: center;
 }
 
-.dateHeart{
-  display: flex;
-  justify-content: space-between;
-}
-.dateHeart {
-
-}
 
 img {
-  height: 200px;
-  width: 200px;
+  height: 300px;
+  width: 300px;
   object-fit: cover;
 }
+
+.ElseStatment {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
 </style>
